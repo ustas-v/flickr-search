84594ff98525c6868b6495d42905a819c8df59bd
Vladimir Bogutskiy <ustas.v@gmail.com>
2017-02-11 12:53:24 +0300

    Add main controller & routes

---
+++ app/controllers/searches_controller.rb
@@ -0,0 +1,23 @@
+class SearchesController < ApplicationController
+  ## Modules, constants
+  ## Helpers
+  ## Before, after and other filters
+
+  ## Loading and authorize resource(s)
+  ## Decorate resource(s)
+  ## Responders
+
+  ## Actions
+
+  def index; end
+
+  def photos; end
+
+  ## Protected methods
+  ## Callbacks before, after, other filters and etc
+  protected
+
+  ## Private methods
+  ## Callbacks before, after, other filters and etc
+  private
+end
